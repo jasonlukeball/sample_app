1dc0159252cee00ffbacb47ad0832b001ce2d3b2
Jason Ball <jasonlukeball@me.com>
2015-07-23 16:40:05 +1000

    Changes to tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
+
+  # Empty actions automatically try to render a corresponding view
+
   def home
   end
 
   def help
   end
+
+  def about
+  end
+
+
 end
