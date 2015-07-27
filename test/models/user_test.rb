9bda2dd6d9467866a85dc4dd1c1ec51efc3aeac7
Jason Ball <jasonlukeball@me.com>
2015-07-27 20:54:21 +1000

    Users validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,26 +7,26 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
 
-  test "name should be present" do
+  test 'name should be present' do
     @user.name = "      "
     assert_not @user.valid?
     # returns false if empty string for user.name & test fails
   end
 
 
-  test "email should be present" do
+  test 'email should be present' do
     @user.email = "      "
     assert_not @user.valid?
     # returns false if empty string for user.email & test fails
   end
 
 
-  test "the name should not be too long" do
+  test 'the name should not be too long' do
     # Set user's name to a string 51 characters long
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -34,11 +34,39 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-  test "the email should not be too long" do
+  test 'the email should not be too long' do
     # Set user's email to a string 256 characters long
     @user.email = "a" * 256
     assert_not @user.valid?
     # Returns true, user's email > 255 characters is NOT VALID
+  end
+
+
+  test 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn ]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+      end
+  end
+
+
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+
+  test 'email address should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
 
