de10d5244f79f8b9b02066f6b67b943335a0045f
Jason Ball <jasonlukeball@me.com>
2015-07-29 21:47:13 +1000

    Added sessions controller and new view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+
+  def new
+  end
+
+
+  def create
+    render 'new'
+  end
+
+
+  def destroy
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,30 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
 
-  root                'static_pages#home'
-  get 'help'      =>  'static_pages#help'
-  get 'about'     =>  'static_pages#about'
-  get 'contact'   =>  'static_pages#contact'
+  # Root
+  # ----------------------------------------------------------------
+  root                        'static_pages#home'
 
-  get 'signup'   =>  'users#new'
 
+  # Static Pages
+  # ----------------------------------------------------------------
+  get     'help'          =>  'static_pages#help'
+  get     'about'         =>  'static_pages#about'
+  get     'contact'       =>  'static_pages#contact'
+
+
+  # Users
+  # ----------------------------------------------------------------
+  get     'users/new'
+  get     'signup'        =>  'users#new'
   resources :users
+
+
+  # Sessions
+  # ----------------------------------------------------------------
+  get     'sessions/new'
+  get     'login'         =>  'sessions#new'
+  post    'login'         =>  'sessions#create'
+  delete  'logout'        =>  'sessions#destroy'
 
 end
