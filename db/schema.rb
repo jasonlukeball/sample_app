1b7a6874f81d6b7aa25c7c22cf1700f7a920a4a4
Jason Ball <jasonlukeball@me.com>
2015-07-29 13:52:51 +1000

    Added new user form

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150727110501) do
 
-  create_table "pages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
