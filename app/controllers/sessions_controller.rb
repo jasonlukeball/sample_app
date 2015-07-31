44aae0c06dd36d39a69d73544df4c5fcfa888e9c
Jason Ball <jasonlukeball@me.com>
2015-07-31 16:25:23 +1000

    Modded _header navigation partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,17 @@ class SessionsController < ApplicationController
 
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # User Exists & Authentication Successful
       # Log the user in & redirect to the user's show page
+      # Logic for log_in comes from the SessionsHelper
+      log_in user
+      redirect_to user
     else
       # User does not exist or Authentication Failed
       # Show error message
-      flash.now[:danger] = 'Invalid Username/Password combination'
-      # Go back to login page
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
