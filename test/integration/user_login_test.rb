968fcb8635c49824e2b8f48611c825c218cf09aa
Jason Ball <jasonlukeball@me.com>
2015-08-02 17:59:36 +1000

    Added login with VALID infmoration test

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:jason)
+
+  end
+
+
 
   test 'login with invalid information' do
 
@@ -29,6 +35,29 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     # Returns true if flash is empty after navigating to a new page
 
   end
+
+
+  test 'login with VALID information' do
+
+    # Go to the login view
+    get login_path
+
+    # Test that we're on the correct view
+    assert_template 'sessions/new'
+
+    # Simulate submitting the login form
+    post login_path session: {email: @user.email, password: 'password'}
+
+    # We should be redirected to the user's profile page
+    assert_redirected_to @user
+
+    follow_redirect!
+
+    assert_template 'users/show'
+    # Returns true if flash is empty after navigating to a new page
+
+  end
+
 
 
 end
