adff4593957ba4a0224c3fb6e619d9ee2dd33b29
Jason Ball <jasonlukeball@me.com>
2015-08-07 15:45:18 +1000

    Added password_resets_test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
 
   before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
 
 
   def new
@@ -61,14 +62,19 @@ class PasswordResetsController < ApplicationController
 
 
   private
+
+    # Before filters
+
     # Gets a user from the database based on their email address
     def get_user
       @user = User.find_by(email: params[:email])
+    end
 
+    # Confirm a valid user
+    def valid_user
       unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
         redirect_to root_url
       end
-
     end
 
     def user_params
