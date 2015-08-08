474238cef5928b86e9c3178903938c5ca615d0cd
Jason Ball <jasonlukeball@me.com>
2015-08-08 14:45:08 +1000

    Added microposts to user profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+# Create a user
 User.create!( name: 'Jason Ball',
               email: 'jasonlukeball@me.com',
               password: 'password',
@@ -14,6 +16,7 @@ User.create!( name: 'Jason Ball',
               activated: true,
               activated_at: Time.zone.now )
 
+# Create another 99 users
 99.times do |i|
 
   name      = Faker::Name.name
@@ -25,5 +28,15 @@ User.create!( name: 'Jason Ball',
                   password_confirmation: password,
                   activated: true,
                   activated_at: Time.zone.now )
-
 end
+
+# Create 50 microposts for the first 6 users
+# Get 6 users
+users = User.order(:created_at).take(6)
+
+# Create 50 microposts for each of the first 6 users
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content)}
+end
+
